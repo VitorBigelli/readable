ed8866fb328b59d4f25c91947e801823e0d2a6ed
Vitor <vitorbigelli@hotmail.com>
2017-12-14 15:24:28 -0200

    feat: Add redux-logger

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose} from 'redux'; 
 import thunk from 'redux-thunk'; 
 import reducers from '../reducers/index';
-
+import logger from 'redux-logger'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore( 
 	reducers, 
 	composeEnhancers(
-		applyMiddleware(thunk)
+		applyMiddleware(thunk, logger)
 	)
 );
 
